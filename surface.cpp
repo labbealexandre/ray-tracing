be96a4d22e0ec58c716473d8802539d21f8b65e9
Alexandre Labbé <alexandre.labbe@ensta-paristech.fr>
2019-12-30 20:52:06 +0100

    getColors method

---
+++ surface.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include "surface.hpp"
+
+void OBJECT_BASE_SURFACE::getColors(
+    std::vector<int>& incident,
+    std::vector<int>& reflected,
+    std::vector<int>& transmitted,
+    std::vector<int>& absorbed)
+{
+    reflected = multiply(reflexion_coefficient, incident);
+    transmitted = multiply(transmission_coefficient, incident);
+    absorbed = multiply(absorption_coefficient, incident);
+}
+
+std::vector<int> multiply(std::vector<float> &coef, std::vector<int> &colors) {
+    std::vector<int> res;
+    if (coef.size() != colors.size()) {
+        // TODO : handle error
+    } else {
+        for (int i = 0; i < 3; i++) {
+            res.push_back((int)coef[i]*colors[i]);
+        }
+    }
+    return res;
+}
--- surface.hpp
+++ surface.hpp
@@ -1,22 +1,40 @@
+#include <vector>
+
 class OBJECT_BASE_SURFACE {
     private:
-        float reflexion_coefficient;
-        float transmission_coefficient;
+        std::vector<float> reflexion_coefficient;
+        std::vector<float> transmission_coefficient;
+        std::vector<float> absorption_coefficient;
 
     public:
+        void getColors(
+            std::vector<int>& incident,
+            std::vector<int>& reflected,
+            std::vector<int>& transmitted,
+            std::vector<int>& absorbed
+        );
 
         /** Constructors */
         OBJECT_BASE_SURFACE() {
-            reflexion_coefficient = 1;
-            transmission_coefficient = 1;
+            for (int i = 0; i < 3; i++) {
+                reflexion_coefficient.push_back(0);
+                transmission_coefficient.push_back(0);
+                absorption_coefficient.push_back(1);
+            }
         }
 
-        OBJECT_BASE_SURFACE(float r, float t) {
+        OBJECT_BASE_SURFACE(
+            std::vector<float> &r,
+            std::vector<float> &t,
+            std::vector<float> &a)
+        {
             reflexion_coefficient = r;
             transmission_coefficient = t;
+            absorption_coefficient = a;
         }
 
         /** Destructor */
         ~OBJECT_BASE_SURFACE();
-
 };
+
+std::vector<int> multiply(std::vector<float>&, std::vector<int>&);
