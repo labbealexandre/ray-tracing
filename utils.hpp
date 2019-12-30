b5937beaa5ef0ec22b9800745a21e04cdb5d9194
Alexandre Labbé <alexandre.labbe@ensta-paristech.fr>
2019-12-30 20:19:15 +0100

    order

--- utils.hpp
+++ utils.hpp
@@ -1,9 +1,8 @@
 #include <vector>
 
+/** Overloading of the vector operators */
+
 template <typename T> std::vector<T> operator + (std::vector<T>, std::vector<T>);
 template <typename T> std::vector<T> operator - (std::vector<T>, std::vector<T>);
-
-/** scalar product */
-template <typename T> T operator * (std::vector<T>, std::vector<T>);
-
+template <typename T> T operator * (std::vector<T>, std::vector<T>); // scalar product
 template <typename T> std::vector<T> operator * (T, std::vector<T>);
