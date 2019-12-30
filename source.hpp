824d616341c5ff3ead37aacf05410bf93ef27de9
Alexandre Labbé <alexandre.labbe@ensta-paristech.fr>
2019-12-30 19:14:44 +0100

    constrcutors and destructor

--- source.hpp
+++ source.hpp
@@ -8,4 +8,20 @@ class LIGHT_SOURCE {
     public:
         std::vector<float> getIncidentRay(std::vector<float>);
         std::vector<float> getReflectedRay(std::vector<float> P, std::vector<float> N);
+
+        /** Constructors */
+        LIGHT_SOURCE() {
+            for(int i = 0; i < 3; i++) {
+                position.push_back(0);
+            }
+        }
+
+        LIGHT_SOURCE(float x, float y, float z) {
+            position.push_back(x);
+            position.push_back(y);
+            position.push_back(z);
+        }
+
+        /** Destructor */
+        ~LIGHT_SOURCE();
 };
--- surface.hpp
+++ surface.hpp
@@ -1,7 +1,22 @@
 class OBJECT_BASE_SURFACE {
     private:
         float reflexion_coefficient;
-        float environnement_index;
+        float transmission_coefficient;
 
     public:
+
+        /** Constructors */
+        OBJECT_BASE_SURFACE() {
+            reflexion_coefficient = 1;
+            transmission_coefficient = 1;
+        }
+
+        OBJECT_BASE_SURFACE(float r, float t) {
+            reflexion_coefficient = r;
+            transmission_coefficient = t;
+        }
+
+        /** Destructor */
+        ~OBJECT_BASE_SURFACE();
+
 };
