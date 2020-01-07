5ad2ccf5d0ccf54db918bcbf0012eea66239fff4
Alexandre Labbé <alexandre.labbe@ensta-paristech.fr>
2020-01-07 10:34:56 +0100

    ray

---
+++ ray.hpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class RAY {
+    public:
+        std::vector<float> origin;
+        std::vector<float> direction;
+        std::vector<int> colors;
+
+        RAY() {
+            origin = {0,0, 0};
+            direction = {0, 0, 0};
+            colors = {0, 0, 0}; 
+        };
+
+        RAY(std::vector<float> O, std::vector<float> U, std::vector<int> C) {
+            origin = O;
+            direction = U;
+            colors = C;
+        }
+
+        ~RAY();
+}
