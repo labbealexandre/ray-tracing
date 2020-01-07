e21b76de9b9df310a5bcbaeaf9f44c11765381c1
Alexandre Labbé <alexandre.labbe@ensta-paristech.fr>
2020-01-07 12:06:53 +0100

    run

--- object.cpp
+++ object.cpp
@@ -4,31 +4,36 @@
 #include "object.hpp"
 #include <math.h>
 
-std::vector<int> getIllumination(std::vector<float> L, std::vector<float> N, std::vector<float> V, ){
-    return surface.diffuse_coefficient*(L*N)+surface.specular_coefficient*(V*getReflectedRay(L,N))
+std::vector<float> SCENE_BASE_OBJECT::getIllumination(
+    std::vector<float> L,
+    std::vector<float> N,
+    std::vector<float> V,
+    std::vector<float> R
+){
+    return (L*N)*surface.diffuse_coefficient+(V*R)*surface.specular_coefficient;
 }
 
-std::vector<float> getNormal(std::vector<float> P){
+std::vector<float> SPHERE_OBJECT::getNormal(std::vector<float> P){
         std::vector<float> N;
-    N=(P-centre)/radius;
+    N=normalise(P-center);
 }
 
-std::vector<float> getIntersection(RAY L, int &code){
-    code=0
-    std::vector<float> S=L->origin;
-    std::vector<float> D=L->direction;
+std::vector<float> SPHERE_OBJECT::getIntersection(RAY L, int &code){
+    code=0;
+    std::vector<float> S=L.origin;
+    std::vector<float> D=L.direction;
     std::vector<float> V=S-center;
     float t;
     if (pow(V*D,2)>(V*V-pow(radius,2))) {
       if (V*D<-sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
-        t=-V*D-sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
-        return S+t*D
+        t=-(V*D)-sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+        return S+t*D;
       } else if (V*D<sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
-        t=-V*D+sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
-        return S+t*D
+        t=-(V*D)+sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+        return S+t*D;
       }
     }else {
-      code=-1
+      code=-1;
       return S;
     }
 }
--- object.hpp
+++ object.hpp
@@ -3,6 +3,7 @@
 #include "surface.hpp"
 #include "source.hpp"
 #include "utils.hpp"
+#include "ray.hpp"
 
 class SCENE_BASE_OBJECT {
     protected:
@@ -12,7 +13,12 @@ class SCENE_BASE_OBJECT {
     public:
 
         // TOTEST Now
-        std::vector<int> getIllumination(std::vector<float> L, std::vector<float> N, std::vector<float> V);
+        std::vector<float> getIllumination(
+            std::vector<float> L,
+            std::vector<float> N,
+            std::vector<float> V,
+            std::vector<float> R
+        );
 
         // TODO Later for recursive points
         std::vector<float> getReflectedRayDirection(std::vector<float> V, std::vector<float> N);
@@ -34,7 +40,7 @@ class SPHERE_OBJECT : public SCENE_BASE_OBJECT{
     public:
 
         std::vector<float> getNormal(std::vector<float> P);
-        std::vector<float> getIntersection(RAY L);
+        std::vector<float> getIntersection(RAY L, int&);
 
         /** Constructors */
         SPHERE_OBJECT(std::vector<float> centre, float r) {
