7ad48ed5c5691ceacc0d9fe804be7f77d9fe3f4f
marc <michoux@ensta.fr>
2020-01-14 12:01:50 +0100

    added missing display.hpp

--- display.hpp
+++ display.hpp
@@ -8,19 +8,4 @@
 
 
 /** Construit et sauvegarde l'image **/
-void savePicture( const std::string& filename, int W, int H, const std::vector<std::vector<int>>& nbIters)
-{
-    std::ofstream ofs( filename.c_str(), std::ios::out | std::ios::binary );
-    ofs << "P6\n"
-        << W << " " << H << "\n255\n";
-    for ( int i = 0; i < W * H; ++i ) {
-        double iter = nbIters[i][0];
-        double itev = nbIters[i][1];
-        double iteb = nbIters[i][2];
-        unsigned char r = (unsigned char)((unsigned (iter) & 0xFF));
-        unsigned char b = (unsigned char)((unsigned (iteb) & 0xFF));
-        unsigned char g = (unsigned char)((unsigned( itev) & 0xFF));
-        ofs << r << g << b;
-    }
-    ofs.close();
-}
+void savePicture( const std::string&, int, int, const std::vector<std::vector<int>>&)
