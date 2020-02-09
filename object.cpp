91fc8e5e1e3f05e10be4d5693c0222efaf2125e8
Marc MICHOUX <marc.michoux@ensta-paris.fr>
2020-02-09 22:52:47 +0100

    fixed sphere intersection (totest)

--- object.cpp
+++ object.cpp
@@ -84,14 +84,25 @@ std::vector<float> SPHERE_OBJECT::getIntersection(RAY L, int &code){
     float t;
     if (V*V > pow(radius, 2)) {
       if (pow(V*D,2)>(V*V-pow(radius,2))) {
-        if (V*D<-sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
-          t=-(V*D)-sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
-          return S+t*D;
+        if (sqrt(V*V)>=radius){
+          if (V*D<-sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
+            t=-(V*D)-sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+            return S+t*D;
+          }
+          if (V*D<sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
+            t=-(V*D)+sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+            return S+t*D;
+          }
         }
-        if (V*D<sqrt(pow(V*D,2)-(V*V-pow(radius,2)))) {
-          t=-(V*D)+sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
-          return S+t*D;
+        else
+        {
+          float u,v;
+          u=-(V*D)-sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+          v=-(V*D)+sqrt(pow(V*D,2)-(V*V-pow(radius,2)));
+          if (u>0) return S+u*D;
+          if (v>0) return S+v*D;  
         }
+        
       }
     }
     code = 0;
